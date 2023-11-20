fef3623efe5f84d97c0f5d2c0ea4e3cf8bd2cc4c
RomanNTA <stivi@centrum.cz>
2023-11-20 19:50:49 +0100

    Add files via upload

---
+++ DU-001/src/main/java/cz/robodo/javadevloper/myproject/Main.java
@@ -0,0 +1,13 @@
+package cz.robodo.javadevloper.myproject;
+
+public class Main {
+
+    public static void main(String[] agg){
+        System.out.println("AHOJ JAVO !!!");
+    }
+
+
+// CTRL SHIFT F10
+
+
+}
