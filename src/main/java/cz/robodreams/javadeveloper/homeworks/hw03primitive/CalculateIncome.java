e9e145393badde68552dadfbe5546aaa2a32015d
petr nejedly <petr.nejedly@noxtrcoporation.com>
2023-11-21 18:22:25 +0100

    Add first HW for lecture 3, primitives

---
+++ src/main/java/cz/robodreams/javadeveloper/homeworks/hw03primitive/CalculateIncome.java
@@ -0,0 +1,68 @@
+package cz.robodreams.javadeveloper.homeworks.hw03primitive;
+
+import java.math.BigDecimal;
+
+public class CalculateIncome {
+
+    /**
+     * Calculate daily expensive Note: one day has 8 working hours
+     */
+    public int dailyIncome(int hourlyRate) {
+        int result = 0;
+        // code your solution here
+
+        // last statement will be assigment your temporary result to result variable
+        //======
+        return result;
+    }
+
+    /**
+     * Calculate monthly expensive Note: one day has 8 working hours, one month has 20 working days
+     */
+    public int monthlyIncome(int hourlyRate) {
+        int result = 0;
+        // code your solution here
+
+        // last statement will be assigment your temporary result to result variable
+        //======
+        return result;
+    }
+
+    public double averageExpensiveOnTeamMemberInDay(int testerHourRate, int programmerHourRate, int analystHourRate) {
+        double result = 0;
+        // code your solution here
+
+        // last statement will be assigment your temporary result to result variable
+        //======
+
+        return result;
+    }
+
+    /**
+     * BONUS task
+     */
+    public BigDecimal averageExpensiveOnTeamMemberInDayRoundedToDecimalPlaces(int testerHourRate, int programmerHourRate, int analystHourRate,
+            byte decimalPlace) {
+        BigDecimal result = BigDecimal.ZERO;
+        // code your solution here
+
+        // last statement will be assigment your temporary result to result variable
+        //======
+        return result;
+    }
+
+    /**
+     * Create report with monthExpensives in format: Analyst:$monthRate; Developer:$devMonthRate; Tester:$testerMonthRate
+     * <p>
+     * instead of $... give your numbers be aware of "; " between salary items. Contains ; and space
+     */
+    public String createReportWithMonthExpensive(int testerHourRate, int programmerHourRate, int analystHourRate) {
+        String result = "";
+        // code your solution here
+
+        // last statement will be assigment your temporary result to result variable
+        //======
+        return result;
+    }
+
+}
---
+++ src/test/java/cz/robodreams/javadeveloper/homeworks/hw03primitive/CalculateIncomeTest.java
@@ -0,0 +1,88 @@
+package cz.robodreams.javadeveloper.homeworks.hw03primitive;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import org.assertj.core.data.Percentage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CalculateIncomeTest {
+
+    CalculateIncome calculateIncome = new CalculateIncome();
+
+    @ParameterizedTest
+    @CsvSource({
+            "500,4000",
+            "200,1600",
+            "-200,-1600"
+    })
+    void dailyIncome(int hourlyRate, int expectedResult) {
+
+        assertThat(calculateIncome.dailyIncome(hourlyRate))
+                .isEqualTo(expectedResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "500,80000",
+            "200,32000",
+            "-200,-32000"
+    })
+    void monthlyIncome(int hourlyRate, int expectedResult) {
+        assertThat(calculateIncome.dailyIncome(hourlyRate))
+                .isEqualTo(expectedResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "500,500,500,500.0",
+            "400,300,200,300",
+            "500,523,332,451.6666666666667"
+    })
+    void averageExpensiveOnTeamMemberInDay(int testerHourRate, int programmerHourRate, int analystHourRate, double expected) {
+
+        assertThat(calculateIncome.averageExpensiveOnTeamMemberInDay(testerHourRate, programmerHourRate, analystHourRate))
+                .isEqualTo(expected);
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "500,523,332,451.67",
+            "200,215,246,220,33"
+    })
+    void averageExpensiveOnTeamMemberInDay_decimalPart(int testerHourRate, int programmerHourRate, int analystHourRate, double expected) {
+
+        assertThat(calculateIncome.averageExpensiveOnTeamMemberInDay(testerHourRate, programmerHourRate, analystHourRate))
+                .isCloseTo(expected, Percentage.withPercentage(1.0));
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "500,523,332,2,451.67",
+            "500,523,332,0,452",
+            "200,215,246,4,220.3333"
+    })
+    void averageExpensiveOnTeamMemberInDayRoundedToDecimalPlaces(int testerHourRate, int programmerHourRate, int analystHourRate, byte decimalPlace,
+            BigDecimal expected) {
+
+        BigDecimal result = calculateIncome.averageExpensiveOnTeamMemberInDayRoundedToDecimalPlaces(testerHourRate, programmerHourRate,
+                analystHourRate, decimalPlace);
+
+        assertThat(result)
+                .isEqualTo(expected);
+    }
+
+    @Test
+    void createReportWithMonthExpensive() {
+
+        String reportWithMonthExpensive = calculateIncome.createReportWithMonthExpensive(300, 500, 400);
+
+        assertThat(reportWithMonthExpensive)
+                .isEqualTo("Analyst:64000; Developer:80000; Tester:48000");
+    }
+
+}
