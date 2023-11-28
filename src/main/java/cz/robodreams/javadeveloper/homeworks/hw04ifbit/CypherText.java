068f12956b356ece850a0ee0c883568786b057e4
petr nejedly <petr.nejedly@noxtrcoporation.com>
2023-11-28 17:54:29 +0100

    Add hw for lecture 4

---
+++ src/main/java/cz/robodreams/javadeveloper/homeworks/hw04ifbit/CypherText.java
@@ -0,0 +1,32 @@
+package cz.robodreams.javadeveloper.homeworks.hw04ifbit;
+
+public class CypherText {
+
+    /**
+     *
+     * Encrypt plain number by xor bit method
+     *
+     */
+    public int encryptByXor(int plain, int key) {
+        int result = 0;
+        // code your solution here
+
+
+        // last statement will be assigment your temporary result to result variable
+        //======
+        return result;
+
+    }
+
+    public int decryptByXor(int cypher, int key) {
+        int result = 0;
+        // code your solution here
+        // last statement will be assigment your temporary result to result variable
+        //======
+        return result;
+    }
+
+
+
+
+}
