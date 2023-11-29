fb0c8e534aa3100a955339cbb66b69f580b7f699
RomanNTA <roman68sliva@gmail.com>
2023-11-29 21:34:44 +0100

    Work completed ... my lord :)

--- src/main/java/cz/robodreams/javadeveloper/homeworks/hw04ifbit/BMICalculator.java
+++ src/main/java/cz/robodreams/javadeveloper/homeworks/hw04ifbit/BMICalculator.java
@@ -28,6 +28,47 @@ public class BMICalculator {
         String result = "Velmi vážná podvýživa";
         // code your solution here
 
+        // převod na metry
+        double dHeight = height / 100.0;
+
+        // Výpočet BMI podle vzorce
+        double dBmi = weight / (dHeight * dHeight);
+
+        // Kontrolní výpis
+//        System.out.println("Zadaní ...................");
+//        System.out.println("height   = " + height);
+//        System.out.println("weight   = " + weight);
+//        System.out.println("dHeight  = " + dHeight);
+//        System.out.println("BMI      = " + dBmi);
+
+        if (dBmi < 0) {
+            result = "Chyba zadání. Prosím, zkontroluj hmotnost a výšku.";
+
+        } else if (dBmi > 0 && dBmi <= 15) {
+            result = "Velmi vážná podvýživa";
+
+        } else if (dBmi > 15 && dBmi <= 16 ){
+            result = "Vážná podvýživa";
+
+        } else if (dBmi > 16 && dBmi <= 18.5) {
+            result = "Podvýživa";
+
+        } else if (dBmi > 18.5 && dBmi <= 25){
+            result = "Normální";
+
+        } else if (dBmi > 25 && dBmi <= 30) {
+            result = "Nadváha";
+
+        } else if (dBmi > 30 && dBmi <= 35) {
+            result = "Nadváha I. stupně (Střední obezita)";
+
+        } else if (dBmi > 35 && dBmi <= 40) {
+            result = "Nadváha II. stupně (Vážná obezita)";
+
+        } else if (dBmi > 40) {
+            result = "Nadváha III. stupně (Velmi vážná obezita)";
+        }
+
         // assign your result to result variable
         //======
         return result;
