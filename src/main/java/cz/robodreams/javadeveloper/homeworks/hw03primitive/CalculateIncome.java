c44e5d4e783fc9003142ed7ab62e6383c6d3c9bc
JanaJMT <tokarova@szsmi.sk>
2023-12-02 17:37:45 +0100

    cela uloha

--- src/main/java/cz/robodreams/javadeveloper/homeworks/hw03primitive/CalculateIncome.java
+++ src/main/java/cz/robodreams/javadeveloper/homeworks/hw03primitive/CalculateIncome.java
@@ -6,14 +6,13 @@ public class CalculateIncome {
 
     /**
      * Calculate daily expensive Note: one day has 8 working hours
+     * hourlyRate ak je hodinova sadzba, za 8 hodín potom bude výsledok
      */
     public int dailyIncome(int hourlyRate) {
         int result = 0;
         // code your solution here
-
         // last statement will be assigment your temporary result to result variable
-
-        
+         result = 8 * hourlyRate;
         //======
         return result;
     }
@@ -24,19 +23,18 @@ public class CalculateIncome {
     public int monthlyIncome(int hourlyRate) {
         int result = 0;
         // code your solution here
-
         // last statement will be assigment your temporary result to result variable
         //======
+        result = 8 * 20 * hourlyRate;
         return result;
     }
 
     public double averageExpensiveOnTeamMemberInDay(int testerHourRate, int programmerHourRate, int analystHourRate) {
         double result = 0;
         // code your solution here
-
         // last statement will be assigment your temporary result to result variable
         //======
-
+        result = (testerHourRate + programmerHourRate + analystHourRate)/3;
         return result;
     }
 
@@ -47,9 +45,9 @@ public class CalculateIncome {
             byte decimalPlace) {
         BigDecimal result = BigDecimal.ZERO;
         // code your solution here
-
         // last statement will be assigment your temporary result to result variable
         //======
+        result = new BigDecimal((testerHourRate + programmerHourRate + analystHourRate) * 8 / 3);
         return result;
     }
 
@@ -61,9 +59,9 @@ public class CalculateIncome {
     public String createReportWithMonthExpensive(int testerHourRate, int programmerHourRate, int analystHourRate) {
         String result = "";
         // code your solution here
-
         // last statement will be assigment your temporary result to result variable
         //======
+        result = String.format("Tester has: %d Programmer has: %d Analyst has: %d", testerHourRate, programmerHourRate, analystHourRate);
         return result;
     }
 
