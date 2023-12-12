11ab27b809622f1a5b95bef392d0972baf85ca59
petr nejedly <petr.nejedly@noxtrcoporation.com>
2023-12-12 23:08:10 +0100

    add hw08 Array

---
+++ src/main/java/cz/robodreams/javadeveloper/homeworks/hw08array/ArraySearch.java
@@ -0,0 +1,34 @@
+package cz.robodreams.javadeveloper.homeworks.hw08array;
+
+public class ArraySearch {
+
+    /**
+     *B
+     *
+     *
+     * Return index of  element in sorted array (from lowest to highest) using linear search If not found return -1;
+     *
+     * @param arr
+     * @param element
+     * @return
+     */
+    public int linearSearch(int[] arr, int element) {
+        return -1;
+    }
+
+
+    /**
+     *BONUS TASK
+     *
+     *
+     * Return index of  element in sorted array (from lowest to highest) using binary search If not found return -1;
+     *
+     * @param arr
+     * @param element
+     * @return
+     */
+    public int binarySearch(int[] arr, int element) {
+        return -1;
+    }
+
+}
