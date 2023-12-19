a36bff856ec929ab6058b5c0b06b88e62b382998
petr nejedly <petr.nejedly@noxtrcoporation.com>
2023-12-19 22:33:24 +0100

    init commit for project

---
+++ src/main/java/cz/robodreams/javadeveloper/project/Main.java
@@ -0,0 +1,9 @@
+package cz.robodreams.javadeveloper.project;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello project");
+    }
+
+}
---
+++ src/test/java/cz/robodreams/javadeveloper/project/MainTest.java
@@ -0,0 +1,12 @@
+package cz.robodreams.javadeveloper.project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void main() {
+    }
+}
