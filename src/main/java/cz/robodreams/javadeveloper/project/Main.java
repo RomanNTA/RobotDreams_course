fab06b0e9e2cf683f3a94e5998d893e2ee58bf11
RomanNTA <roman68sliva@gmail.com>
2024-01-02 15:07:41 +0100

    :)

--- src/main/java/cz/robodreams/javadeveloper/project/Main.java
+++ src/main/java/cz/robodreams/javadeveloper/project/Main.java
@@ -3,7 +3,7 @@ package cz.robodreams.javadeveloper.project;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello project");
+        System.out.println("Hello virtual libray :)");
     }
 
 }
