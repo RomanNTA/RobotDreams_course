007d0716b139fcfd8ef230a2a635942459adba34
RomanNTA <roman68sliva@gmail.com>
2024-02-15 18:42:11 +0100

    :)

--- src/main/java/cz/robodreams/javadeveloper/project/books/BooksGenerator.java
+++ src/main/java/cz/robodreams/javadeveloper/project/books/BooksGenerator.java
@@ -35,10 +35,19 @@ public class BooksGenerator implements IBooksGenerator {
 
     public IBook getBook(int id) {
 
-        return new Book(id, allBooks.getString(id, 0), allBooks.getString(id, 1),
-                allBooks.getInteger(id, 2), allBooks.getInteger(id, 3), allBooks.getString(id, 4),
-                allBooks.getString(id, 5), allBooks.getString(id, 6), allBooks.getString(id, 7),
-                allBooks.getString(id, 8), allBooks.getInteger(id, 9));
+        return new Book(
+            id,
+            allBooks.getString(id, IBook.TITLE),
+            allBooks.getString(id, IBook.AUTHOR),
+            allBooks.getInteger(id, IBook.NUMBEROFPAGES),
+            allBooks.getInteger(id, IBook.PRICE),
+            allBooks.getString(id, IBook.ISBN),
+            allBooks.getString(id, IBook.EAN),
+            allBooks.getString(id, IBook.CUSTODY),
+            allBooks.getString(id, IBook.GENRE),
+            allBooks.getString(id, IBook.PUBLISHER),
+            allBooks.getInteger(id, IBook.PROFIT));
+
     }
 
     public Integer getCountOfBooks() {
--- src/main/java/cz/robodreams/javadeveloper/project/books/IBook.java
+++ src/main/java/cz/robodreams/javadeveloper/project/books/IBook.java
@@ -5,6 +5,18 @@ import cz.robodreams.javadeveloper.project.lending.ALoan;
 public interface IBook {
 
 
+    int TITLE = 0;
+    int AUTHOR = 1;
+    int NUMBEROFPAGES = 2;
+    int PRICE = 3;
+    int ISBN = 4;
+    int EAN = 5;
+    int CUSTODY = 6;
+    int GENRE = 7;
+    int PUBLISHER = 8;
+    int PROFIT = 9;
+
+
     void show(Boolean shortLongFormat);
 
     String getShortInfo();
