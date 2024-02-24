a91b4762af0fbef89f9337ce12efb4b39f66e86c
RomanNTA <roman68sliva@gmail.com>
2024-02-24 18:48:59 +0100

    Revert ":)"
    This reverts commit fab06b0e

--- src/main/java/cz/robodreams/javadeveloper/project/Main.java
+++ src/main/java/cz/robodreams/javadeveloper/project/Main.java
@@ -3,15 +3,7 @@ package cz.robodreams.javadeveloper.project;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println();
-        System.out.println();
-        System.out.println("   Dnes byla slavnostně otevřená nová knihovna ve Švestkové Lhotě.");
-        System.out.println();
-        System.out.println();
-
-        ICityLibrary library = new CityLibrary();
-        library.oneMonth();
+        System.out.println("Hello project");
     }
 
 }
